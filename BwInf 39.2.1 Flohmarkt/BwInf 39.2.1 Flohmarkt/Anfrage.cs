7c7bebf623c8b0c09627c1ec4a187b1ecf76ec4b
JD-Walli <jakov@wazungu.eu>
2021-02-03 23:07:32 +0100

    Update Anfrage.cs

--- BwInf 39.2.1 Flohmarkt/BwInf 39.2.1 Flohmarkt/Anfrage.cs
+++ BwInf 39.2.1 Flohmarkt/BwInf 39.2.1 Flohmarkt/Anfrage.cs
@@ -6,18 +6,31 @@ using System.Threading.Tasks;
 
 namespace BwInf_39._2._1_Flohmarkt {
     class Anfrage {
-        int id;
-        int mietbeginn;
-        int mietende;
-        int mietdauer;
-        int länge;
+        public int id;
+        public int mietbeginn;
+        public int mietende;
+        public int mietdauer;
+        public int länge;
+        public int position;
 
-        public Anfrage(int id, int mietbeginn, int mietende, int länge) {
+        public Anfrage(int id, int mietbeginn, int mietende, int länge, int position) {
             this.id = id;
             this.mietbeginn = mietbeginn;
             this.mietende = mietende;
             this.länge = länge;
             mietdauer = mietende - mietbeginn;
+            this.position = position;
         }
+
+        public int überschneidung(Anfrage afr2) {
+            int xOverlap = Math.Min(position + länge, afr2.position + länge)-Math.Max(position, afr2.position);
+            int yOverlap = Math.Min(mietende, afr2.mietende)-Math.Max(mietbeginn, afr2.mietbeginn);
+            if (xOverlap > 0 && yOverlap > 0) {
+                return xOverlap * yOverlap;
+            }
+            else { return 0; }
+        }
+
+        public Anfrage clone() { return new Anfrage(this.id, this.mietbeginn, this.mietende, this.länge, this.position); }
     }
 }
