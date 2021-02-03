646b5c5a4ea7fa8455c6c56d11a83951db2cd7f4
JD-Walli <jakov@wazungu.eu>
2021-02-03 23:08:00 +0100

    meta

--- BwInf 39.2.1 Flohmarkt/BwInf 39.2.1 Flohmarkt/Program.cs
+++ BwInf 39.2.1 Flohmarkt/BwInf 39.2.1 Flohmarkt/Program.cs
@@ -7,15 +7,22 @@ using System.Threading.Tasks;
 namespace BwInf_39._2._1_Flohmarkt {
     class Program {
         static void Main(string[] args) {
+            simulatedAnnealing simAnn = new simulatedAnnealing(readData(1), 1000, 10, 100);
+            simAnn.setRandomPos();
+            simAnn.simulate1();
+
+            Console.ReadKey();
         }
 
-        static void readData(int number) {
+        static List<Anfrage> readData(int number) {
             string[] lines = System.IO.File.ReadAllLines(System.IO.Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "/flohmarkt " + number + ".txt");
             List<Anfrage> anfragen = new List<Anfrage>();
-            for(int i=1;i< lines.Length; i++) {
-                String[] line = lines[i].Split(' ');
-                anfragen.Add(new Anfrage(i - 1, int.Parse(line[0].Trim()), int.Parse(line[1].Trim()), int.Parse(line[2].Trim())));
+            for (int i = 1; i < lines.Length; i++) {
+                string[] line = lines[i].Split(' ');
+                anfragen.Add(new Anfrage(i - 1, int.Parse(line[0].Trim()), int.Parse(line[1].Trim()), int.Parse(line[2].Trim()),0));
             }
+            Console.WriteLine(anfragen.Count + "  " + lines.Length);
+            return anfragen;
         }
     }
 }
