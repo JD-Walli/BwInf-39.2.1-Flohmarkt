a8a5e2da0336d70058552710e23ecf7d19356292
JD-Walli <jakov@wazungu.eu>
2021-02-03 19:24:28 +0100

    initial commit
    init

---
+++ BwInf 39.2.1 Flohmarkt/BwInf 39.2.1 Flohmarkt/Anfrage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BwInf_39._2._1_Flohmarkt {
+    class Anfrage {
+        int id;
+        int mietbeginn;
+        int mietende;
+        int mietdauer;
+        int länge;
+
+        public Anfrage(int id, int mietbeginn, int mietende, int länge) {
+            this.id = id;
+            this.mietbeginn = mietbeginn;
+            this.mietende = mietende;
+            this.länge = länge;
+            mietdauer = mietende - mietbeginn;
+        }
+    }
+}
---
+++ BwInf 39.2.1 Flohmarkt/BwInf 39.2.1 Flohmarkt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BwInf_39._2._1_Flohmarkt {
+    class Program {
+        static void Main(string[] args) {
+        }
+
+        static void readData(int number) {
+            string[] lines = System.IO.File.ReadAllLines(System.IO.Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "/flohmarkt " + number + ".txt");
+            List<Anfrage> anfragen = new List<Anfrage>();
+            for(int i=1;i< lines.Length; i++) {
+                String[] line = lines[i].Split(' ');
+                anfragen.Add(new Anfrage(i - 1, int.Parse(line[0].Trim()), int.Parse(line[1].Trim()), int.Parse(line[2].Trim())));
+            }
+        }
+    }
+}
